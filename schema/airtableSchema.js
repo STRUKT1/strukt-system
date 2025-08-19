90b77f4d65a61a8abdda2d399168e1bc964d1c7d
STRUKT1 <hello@strukt.fit>
2025-08-19 21:08:07 +0100

    Create airtableSchema.js

---
+++ schema/airtableSchema.js
@@ -0,0 +1,42 @@
+{
+  "baseId": "appQfzQUbWtJj8Ck6",
+  "tables": {
+    "User": {
+      "id": "tbl87AICCbvbgrLCY",
+      "fields": {
+        "FULL_NAME": "fld1ZCiTpDGafzo9i",
+        "EMAIL_ADDRESS": "fldgyVjQJc389lqNA",
+        "EMAIL_LOWERCASE": "fldoPlhwKbwIKU3Ad",
+        "ONBOARDING_COMPLETE": "fld3Uly8Ju8XUg44G",
+        "RECORD_ID_FORMULA": "fldGZDVHktuEn2zQh"
+      }
+    },
+    "Meals": {
+      "id": "tblWLkTKkxkSEcySD",
+      "fields": {
+        "USER_LINK": "fldWvzars92kZ6fZu",
+        "DATE": "fld9KC4EDsgjsMAPa",
+        "TYPE": "fldmvAbVSaBGVNzqS",
+        "DESCRIPTION": "fldtWww1qbE9yJWte",
+        "CALORIES": "fldQXr3rOcMaBYjOC",
+        "PROTEIN": "fldhts0NWA2ekG60o",
+        "CARBS": "fldOZFMZG8tekyglr",
+        "FATS": "fldtH1QNiyM8ZaX4V",
+        "CONFIRMED": "fldxsrP11ycVMVFKK"
+      }
+    },
+    "Workouts": {
+      "id": "tblgqvIqFetN2s23J",
+      "fields": {
+        "USER_LINK": "fldUuYZtmkiycOVnb",
+        "DATE": "fldzVeaYTUHMxMDd9",
+        "TYPE": "fld9xRDtOz1mBkDQ5",
+        "DESCRIPTION": "fldKkhKomMg3Cf108",
+        "DURATION_MIN": "fldaij5HlQKv8gMcT",
+        "CALORIES": "fld2muGFVrfM0xHmI",
+        "NOTES": "fld1aEpGu5H8DWPxY",
+        "CONFIRMED": "fldS3LfnROZ9adMAo"
+      }
+    }
+  }
+}
